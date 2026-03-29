9ab9a4290a8e5e5f403b3939f5ab50644d881c87
Balagondla Bhanu Prasad <bhanubalagondla@gmail.com>
2026-03-29 09:56:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value;
+
+  if (task === "") return;
+
+  let li = document.createElement("li");
+  li.innerText = task;
+
+  // Mark as complete
+  li.onclick = function () {
+    li.style.textDecoration = "line-through";
+  };
+
+  // Delete button
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "Delete";
+
+  delBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(delBtn);
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
